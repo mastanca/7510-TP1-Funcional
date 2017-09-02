5b6835a999e7eff316bd28a763c30443cda3116e
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-09-02 17:19:01 -0300

    Invalid database parsing ok

--- src/database_parser.clj
+++ src/database_parser.clj
@@ -2,13 +2,22 @@
 
 (require '[clojure.string :as str])
 
+(defn- line-valid?
+  [s]
+  (= (subs s (- (.length s) 1) (.length s)) ".")
+  )
+
+(defn- remove-trailing-char
+  [s]
+  (subs s 0 (- (.length s) 1))
+  )
+
 (defn parse-database-string
   "Parses a given database string, returning a list of facts or nil if database is corrupt"
   [database]
-  (def splitted-string
-    (filter
-      (fn [coll] (not= coll ""))
-      (filter (fn [coll] (not= coll "\n"))
-              (str/split (str/join "" (str/split database #"\.")) #" "))))
-  (map str/trim-newline splitted-string)
+  (def splitted-elements
+    (filter (fn [coll] (not= coll ""))
+            (str/split (str/join "" (str/split database #"\n")) #" ")))
+  (if (not-every? line-valid? splitted-elements) nil (map remove-trailing-char splitted-elements))
   )
+
