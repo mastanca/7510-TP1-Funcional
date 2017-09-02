2dcfc56a5d490ccb9f56c6e320cc0e87b2fa88d8
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-09-02 19:24:11 -0300

    Fix mixed identation

--- test/parent_database_test.clj
+++ test/parent_database_test.clj
@@ -3,20 +3,20 @@
             [logical-interpreter :refer :all]))
 
 (def parent-database "
-	varon(juan).
-	varon(pepe).
-	varon(hector).
-	varon(roberto).
-	varon(alejandro).
-	mujer(maria).
-	mujer(cecilia).
-	padre(juan, pepe).
-	padre(juan, pepa).
-	padre(hector, maria).
-	padre(roberto, alejandro).
-	padre(roberto, cecilia).
-	hijo(X, Y) :- varon(X), padre(Y, X).
-	hija(X, Y) :- mujer(X), padre(Y, X).
+  varon(juan).
+  varon(pepe).
+  varon(hector).
+  varon(roberto).
+  varon(alejandro).
+  mujer(maria).
+  mujer(cecilia).
+  padre(juan, pepe).
+  padre(juan, pepa).
+  padre(hector, maria).
+  padre(roberto, alejandro).
+  padre(roberto, cecilia).
+  hijo(X, Y) :- varon(X), padre(Y, X).
+  hija(X, Y) :- mujer(X), padre(Y, X).
 ")
 
 (deftest parent-database-fact-test
