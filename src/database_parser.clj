4bda01a352055c8c529ea00ab80e0c9c9d6e0f06
Martin Stancanelli <martin.stancanelli@mercadolibre.com>
2017-09-07 00:14:57 -0300

    Use trim

--- src/database_parser.clj
+++ src/database_parser.clj
@@ -4,22 +4,19 @@
 (require '[input-validator])
 (require '[rules-and-facts])
 
+(def rule-separator ":-")
+
 (defn- remove-trailing-char
   [s]
   (subs s 0 (- (.length s) 1))
-  )
-
-(defn- remove-first-two-chars
-  [s]
-  (subs s 2 (.length s))
   )
 
 (defn- create-rules-and-facts
   "Creates a RulesAndFacts record from the given parsed db"
   [parsed-db]
   (let [rulesAndFacts (rules-and-facts/->RulesAndFacts
-                        (filter #(str/includes? % ":-") parsed-db)
-                        (filter #(not (str/includes? % ":-")) parsed-db))]
+                        (filter #(str/includes? % rule-separator) parsed-db)
+                        (filter #(not (str/includes? % rule-separator)) parsed-db))]
     rulesAndFacts
     )
   )
@@ -28,10 +25,9 @@
   "Parses a given database string, returning a RulesAndFacts record or nil if database is corrupt"
   [database]
   (let [splitted-elements
-        (map remove-first-two-chars
+        (map str/trim
              (remove str/blank? (str/split database #"\n")))]
     (if (not-every? #(input-validator/valid-input? % ".") splitted-elements)
       nil
       (create-rules-and-facts (map remove-trailing-char splitted-elements))))
   )
-
